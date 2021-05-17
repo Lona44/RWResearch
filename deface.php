6c48e9299d5257de55ee9db66aa5c27bd74111ce
Lona <lonamafaufau@gmail.com>
2021-05-17 18:39:03 +1200

    Create deface.php

---
+++ deface.php
@@ -0,0 +1,20 @@
+<?php
+  $defacegif = "hax0rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.gif";
+?>
+
+<html>
+  <head>
+    <style type="text/css">
+      body {
+        background-image: url('<?php echo $defacegif;?>');
+      }
+    </style>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <title> RANSOM </title>
+  </head>
+  
+  <body>
+  </body>
+</html>
